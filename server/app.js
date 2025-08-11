213e0d079c80a2ab016a8154155abdb50af673a6
Kancherla Gnanesh Kumar <kancherlagnaneshkumar574@gmai.com>
2025-08-11 10:37:19 +0530

    xs

--- server/app.js
+++ server/app.js
@@ -52,3 +52,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+app.use((req, res, next) => {
+  if (!res.getHeader('Access-Control-Allow-Origin')) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://gnanesh-expense-tracker.netlify.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  }
+  next();
+});
